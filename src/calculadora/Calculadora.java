8d4d2ece37848d820a5cba9d57b75b52b10fc43b
GabrielGuzman05 <g.guzman05@ufromail.cl>
2017-03-21 13:56:23 -0300

    ver3
    Ingreso de datos, con validacion, en caso de que se equivoque el
    programa sigue hacia adelante de todas formas.
    Generar loops para volver a preguntar por el dato, division por 0 no
    conseguida.
    Generar metodos en interfaz para distribuir las acciones.

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -14,10 +14,6 @@ public class Calculadora {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
     private double num1;
     private double num2;
     
--- src/calculadora/Interfaz.java
+++ src/calculadora/Interfaz.java
@@ -4,12 +4,62 @@
  * and open the template in the editor.
  */
 package calculadora;
-
+import java.util.Scanner;
+import java.util.InputMismatchException;
 /**
  *
  * @author Gabriel
  */
 public class Interfaz {
-    Calculadora myCalculadora = new Calculadora();
-    
+    static Calculadora myCalculadora = new Calculadora();
+    public static void main(String[] args) {
+        System.out.println("Ingrese dos numeros");
+        double n1=0;
+        double n2=0;
+        try{
+            Scanner leer = new Scanner(System.in);
+            n1=leer.nextDouble();
+        }catch(InputMismatchException e){
+        
+        }
+        try{
+            Scanner leer = new Scanner(System.in);
+            n1=leer.nextDouble();
+        }catch(InputMismatchException e){
+        
+        }
+        myCalculadora.setNum1(n1);
+        myCalculadora.setNum2(n2);
+        
+        System.out.println("Ingrese que opercaion quiere hacer");
+        System.out.println("1 para suma - 2 para resta");
+        System.out.println("3 para dividir - 4 para multiplicar");
+        
+        int option=0;
+        try{
+            Scanner leer = new Scanner(System.in);
+            option=leer.nextInt();
+        }catch(InputMismatchException e){
+        
+        }
+        String resultado="El resultado es ";
+        switch(option){
+            case 1:
+                resultado+=myCalculadora.sumar();
+                break;
+                
+            case 2:
+                resultado+=myCalculadora.restar();
+                break;    
+        
+            case 3:
+                resultado+=myCalculadora.dividir();
+                break;
+                
+            case 4:
+                resultado+=myCalculadora.multiplicar();
+                break;
+        }
+        System.out.println(resultado);
+    }
 }
