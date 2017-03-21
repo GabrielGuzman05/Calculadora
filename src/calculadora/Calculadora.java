41e02c56be56b26e106026f2f2bea024a85886ba
GabrielGuzman05 <g.guzman05@ufromail.cl>
2017-03-21 13:19:01 -0300

    ver2
    Implementados los metodos de las operaciones y creado

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -18,4 +18,32 @@ public class Calculadora {
         // TODO code application logic here
     }
     
+    private double num1;
+    private double num2;
+    
+    public void setNum1(double num){
+        num1=num;
+    }
+    public void setNum2(double num){
+        num2=num;
+    }
+    public double getNum1(){
+        return num1;
+    }
+    public double getNum2(){
+        return num2;
+    }
+    
+    public double sumar(){
+        return (num1+num2);
+    }
+    public double restar(){
+        return (num1-num2);
+    }
+    public double dividir(){
+        return (num1/num2);
+    }
+    public double multiplicar(){
+        return (num1*num2);
+    }
 }
